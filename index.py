b9bcb0a194bc40ca94d448c02cc5a24e0c2f2111
Jemimah Nthenya <jemimahogonda@gmail.com>
2024-02-27 16:35:53 +0300

    add files

---
+++ index.py
@@ -0,0 +1,19 @@
+#Addition
+let x=70
+let y=12
+print(x+y)
+
+#Subtraction
+let x=100
+let y=60
+print(x-y)
+
+#Division
+let x=50
+let y=10
+print(x/y)
+
+#Multiplication
+let x=15
+let y=5
+print(x*y)
